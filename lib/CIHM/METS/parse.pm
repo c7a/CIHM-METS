23a5c411732971cbe462334869b4221db014bac8
Russell McOrmond <russell.mcormond@canadiana.ca>
2017-05-24 12:42:10 -0400

    Added missing files to manifest, increased version number so distribution file would have new version.  Deployed into production environment as part of #1

--- lib/CIHM/METS/parse.pm
+++ lib/CIHM/METS/parse.pm
@@ -18,12 +18,12 @@ CIHM::METS::parse - Parse METS records that conform to the Canadiana Application
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
